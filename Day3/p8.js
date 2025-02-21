731317cd66eb571c75980e9e4890dca0b586eee0
007divyanshu <pandeydivyanshu1509@gmail.com>
2025-02-21 15:37:16 +0530

    day4

--- Day3/p8.js
+++ Day3/p8.js
@@ -1,42 +1,43 @@
-const http = require('http');
-
-const server=http.createServer(async(req,res) => {
+const http=require('http')
+const server=http.createServer(async(req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html'});
-    const data=await fetch ("https://fakestoreapi.com/products");
+    const data=await fetch("https://fakestoreapi.com/products");
     const products=await data.json();
-    const myhtml=`<html>
+    const myhtml = `<html>
     <head>
-    <title>Product</title>
-   <style>
-    .container {
-        border: 1px solid black;
-        background-color: aqua;
-        padding: 10px;
-        margin: 10px;
-        width: fit-content;
-         }
+    <title>Products</title>
+    <style>
+    .container{
+    border:1px solid black;
+    background-color:aqua;}
     </style>
     </head>
     <body>
     <h1>Products</h1>
     ${
         products.map((product)=>{
-            return `<div classname="container">
+            return `<div class="container>"
             <h2>Title: ${product.title}</h2>
-            <p>${product.description}</p>
-            <p>${product.price}</p>
-            <img src "${product.image}" width="200" height="250" alt="${product.title}">
-            </div>`
+            <p>Description: ${product.description}</p>
+            <p>Price: ${product.price}</p>
+            <img src = "${product.image}" width="200"  height="250" alt= "${product.title} ">
+            </div>         
+             `
         })
     }
     </body>
-    </html>`;
+    </html>`
+
+
     res.end(myhtml);
+    
+
 })
 
+
 server.listen(9000,(err)=>{
-    if(err) 
+    if (err)
         console.log(err);
     else
-        console.log('server is running on port 9000');
-})
+       console.log("server is running at http://localhost:9000/ ");
+});
