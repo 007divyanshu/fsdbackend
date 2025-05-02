9f2732d65eadeeaf56ee40d692d524fc54534f0c
007divyanshu <pandeydivyanshu1509@gmail.com>
2025-05-02 14:16:03 +0530

    day7

--- Day7/Book-App/Frontend/src/components/ViewBook.jsx
+++ Day7/Book-App/Frontend/src/components/ViewBook.jsx
@@ -7,7 +7,7 @@ const ViewBook = () => {
   },[])
   const handleview = async () => {
     try {
-      const res = await axios.get('http://localhost:9000/books');
+      const res = await axios.get('https://fsdbackend-3-rvoi.onrender.com/books');
       setBooks(res.data);
     }
     catch (err) {
